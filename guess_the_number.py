67bd60f4c0af6396843c1a16d449010cb7a0a44d
GusevIlya <135234492+GusevIlya@users.noreply.github.com>
2024-04-23 11:44:26 +0300

    Add files via upload

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,5 +1,5 @@
 from random import randint
-
+import os
 
 def is_valid(string):
     if string.isdigit():
@@ -9,31 +9,36 @@ def is_valid(string):
 
 
 print('Добро пожаловать в игру "Угадай число"')
-print('Я загадал число от 1 до 100. Попробуй угадать его.')
 
-num = randint(1, 100)
+answer = 'д'
+while answer == 'д':
+    num = randint(1, 100)
+    print('Я загадал число от 1 до 100. Попробуй угадать его.')
+    flag = True
+    cnt = 1
 
-flag = True
-cnt = 1
-
-while flag:
-    user_num = input(f'Попытка {cnt}. Введите число от 1 до 100: ')
-    if is_valid(user_num):
-        user_num = int(user_num)
-        if user_num > num:
-            print('Мое число меньше')
-            cnt += 1
-        elif user_num < num:
-            print('Мое число больше')
-            cnt += 1
+    while flag:
+        user_num = input(f'Попытка {cnt}. Введите число от 1 до 100: ')
+        if is_valid(user_num):
+            user_num = int(user_num)
+            if user_num > num:
+                print('Мое число меньше')
+                cnt += 1
+            elif user_num < num:
+                print('Мое число больше')
+                cnt += 1
+            else:
+                print('Угадали!!!')
+                flag = False
         else:
-            print('Угадали!!!')
-            flag = False
-    else:
-        print('Нет, нужно целое число от 1 до 100')
+            print('Нет, нужно целое число от 1 до 100')
+
+    answer = input('Сыграем еще раз? да - д, нет - н: ')
+    os.system('cls')
 
 print('Спасибо за игру!')
 input('Для выхода нажмите Enter')
+
 
 
 
